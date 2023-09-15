37018fe48fafc64b958694520bf86f91c55d4b40
Crypto <iamsuryavamsi@gmail.com>
2023-09-15 15:06:48 +0530

    Changes Done

---
+++ DevSecOps/Python/Programs/Function.py
@@ -0,0 +1,11 @@
+def about_me():
+    name = "Crypto"
+    age = "20"
+    favourite_things = "Learning Hacking and Programming"
+    print(f"""
+    Hello i am {name}.
+    I am {age} old.
+    My favourite thing is {favourite_things} :).
+    """)
+
+about_me()
